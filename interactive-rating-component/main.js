a49265c6dc16a339a1b5463a71a75b79e406e0bf
ud39 <ud-gaming@gmx.de>
2023-05-27 22:02:48 +0200

    Adding transition to 'thank you' state and responsivness configuration.

--- interactive-rating-component/main.js
+++ interactive-rating-component/main.js
@@ -1,28 +1,46 @@
 let main = document.querySelector('main')
 let ratingSelection = document.querySelector('em.selected-rating')
+let ratingElements = document.querySelectorAll('.rating:not(.thank-you)');
 let form = document.querySelector('form')
 let thankYou = document.querySelector('section.thank-you')
 let rating = document.querySelectorAll('.rating')
 let radios = form.querySelectorAll('input[type="radio"]')
 let checked = false
 
-
 function submitRating(e) {
-  e.preventDefault()
+  e.preventDefault();
+
+  let checked = false;
+  let transitionCount = 0;
+
+  const handleTransitionEnd = () => {
+    transitionCount++;
+
+    if (transitionCount === ratingElements.length) {
+      ratingElements.forEach(element => {
+        element.removeEventListener('transitionend', handleTransitionEnd);
+        element.style.display = 'none';
+      });
+
+      thankYou.style.display = 'flex';
+    }
+  };
+
   for (let i = 0; i < radios.length; i++) {
-    console.log(radios[i].checked)
     if (radios[i].checked) {
-      checked = true
-      ratingSelection.innerHTML = form.querySelector('input[type="radio"]:checked').value
-      thankYou.style.display = 'flex'
-      rating.forEach( e => e.style.display = 'none')
+      checked = true;
+      ratingSelection.textContent = radios[i].value;
+
+      ratingElements.forEach(element => {
+        element.addEventListener('transitionend', handleTransitionEnd);
+        element.style.opacity = '0';
+      });
+
       break;
     }
-
   }
-    if (!checked) {
-      alert('Please select a rating')
-      console.log('Rating submitted')
-      return
-    } 
+
+  if (!checked) {
+    alert('Please select a rating');
+  }
 }
