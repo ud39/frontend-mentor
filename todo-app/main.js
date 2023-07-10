dd636ac7a621f71c008991fac0ea8af198fdf815
ud39 <ud-gaming@gmx.de>
2023-07-10 19:42:39 +0200

    Solved drag&drop on edge cases and add listener for crossIten function.

--- todo-app/main.js
+++ todo-app/main.js
@@ -1,54 +1,58 @@
-let listItems = document.querySelectorAll(".list-item:not(.list-actions)")
 const listContainer = document.querySelector(".list-items");
 const inputItem = document.querySelector("input[type='text']")
 const listActionsNumberOfItems = document.querySelectorAll("ul.list-actions li:first-child")
 
 
-listItems.forEach(item => {
-  item.draggable = true;
+const makeDropList = (listItems) => {
+  listItems.forEach(item => {
 
-  item.addEventListener("dragstart", e => {
-    const itemId = e.target.dataset.id;
-    e.dataTransfer.setData("text/plain", itemId);
-  });
+    if (item.draggable === true) return
 
-  item.addEventListener("dragover", e => {
-    e.preventDefault();
-  });
+    item.draggable = true;
 
-  item.addEventListener("drop", e => {
-    e.preventDefault();
-    const droppedItem = e.target;
-    const draggedItemId = e.dataTransfer.getData("text");
-    const draggedItem = document.querySelector(`.list-item[data-id="${draggedItemId}"]`);
-    const parentList = draggedItem.parentNode;
-    
-    if (draggedItem.nextElementSibling === droppedItem) {
-      parentList.insertBefore(droppedItem, draggedItem);
-    } else if (draggedItem.previousElementSibling === droppedItem) {
-      parentList.insertBefore(draggedItem, droppedItem);
-    } else {
-      const nextSibling = draggedItem.nextElementSibling
-      parentList.insertBefore(draggedItem, droppedItem);
-      parentList.insertBefore(droppedItem, nextSibling);
-    }    
+    item.addEventListener("dragstart", e => {
+      const itemId = e.target.dataset.id;
+      e.dataTransfer.setData("text/plain", itemId);
+    });
 
-
-  });
-
-
-  const childElements = item.querySelectorAll("span, img");
-  childElements.forEach(childElement => {
-    childElement.addEventListener("dragstart", e => {
+    item.addEventListener("dragover", e => {
       e.preventDefault();
     });
-  });
-});
 
+    item.addEventListener("drop", e => {
+      e.preventDefault();
+      const droppedItem = e.target;
+      const draggedItemId = e.dataTransfer.getData("text");
+      const draggedItem = document.querySelector(`.list-item[data-id="${draggedItemId}"]`);
+      const parentList = draggedItem.parentNode;
+
+      if (draggedItem.nextElementSibling === droppedItem) {
+        parentList.insertBefore(droppedItem, draggedItem);
+      } else if (draggedItem.previousElementSibling === droppedItem) {
+        parentList.insertBefore(draggedItem, droppedItem);
+      } else {
+        const nextSibling = draggedItem.nextElementSibling
+        parentList.insertBefore(draggedItem, droppedItem);
+        parentList.insertBefore(droppedItem, nextSibling);
+      }    
+
+
+    });
+
+
+    const childElements = item.querySelectorAll("span, img");
+    childElements.forEach(childElement => {
+      childElement.addEventListener("dragstart", e => {
+        e.preventDefault();
+      });
+    });
+  });
+}
 
 inputItem.addEventListener("keypress", e => {
   if (e.key === 'Enter' && inputItem.value !== '') {
 
+    let listItems = document.querySelectorAll(".list-item:not(.list-actions)")
     const check = document.createElement("span")
     const checkIMG = document.createElement("img")
     const crossIMG = document.createElement("img")
@@ -58,10 +62,15 @@ inputItem.addEventListener("keypress", e => {
     crossIMG.src = "images/icon-cross.svg"
 
     newListItem.classList.add('list-item')
+    newListItem.dataset.id = listItems === undefined ? 0 : listItems.length + 1
     check.classList.add('check')
 
     crossIMG.addEventListener("click", function () {
       deleteItem(this)
+    })
+
+    check.addEventListener("click", function () {
+      crossItem(this)
     })
 
     check.appendChild(checkIMG)
@@ -69,9 +78,9 @@ inputItem.addEventListener("keypress", e => {
     newListItem.appendChild(document.createTextNode(inputItem.value))
     newListItem.appendChild(crossIMG)
 
-    listItemDropable(newListItem) 
-    if (listItems.length === 0) {
-      listContainer.appendChild(newListItem)
+
+    if (listItems.length === 0 || listItems === undefined) {
+      listContainer.insertBefore(newListItem, listContainer.firstChild)
     } else {
       listItems = document.querySelectorAll(".list-item:not([class*=' '])")
       const lastItem = listItems[listItems.length -1]
@@ -80,9 +89,12 @@ inputItem.addEventListener("keypress", e => {
 
     let numberOfItems = document.querySelectorAll(".list-item").length
     listActionsNumberOfItems.forEach( ul => {
-      ul.innerHTML = numberOfItems
+      ul.innerHTML = numberOfItems + " items left"
     })
-
+    
+    inputItem.value = ''
+    listItems = document.querySelectorAll(".list-item:not(.list-actions)")
+    makeDropList(listItems)
   }
 })
 
@@ -108,7 +120,7 @@ const deleteItem = (node) => {
   node.parentNode.remove()
   let numberOfItems = document.querySelectorAll(".list-item").length
   listActionsNumberOfItems.forEach( ul => {
-    ul.innerHTML = numberOfItems
+    ul.innerHTML = numberOfItems + " items left"
   })
 
 }
